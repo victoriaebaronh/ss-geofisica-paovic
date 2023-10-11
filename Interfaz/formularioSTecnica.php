c1b573967927b6316a1118b67b2dd88c62ef05b4
Victoria Baron <victoriaebaronh@ciencias.unam.mx>
2023-10-11 13:05:23 -0600

    formulario avance

---
+++ Interfaz/formularioSTecnica.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario enviado</title>
+</head>
+<body>
+    <?php
+    
+    function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $area_solicitante = test_input($_POST["area_solicitante"]);
+        $folio = test_input($_POST["folio"]);
+        $responsable_area = test_input($_POST["responsable_area"]);
+        $fecha_solicitud = test_input($_POST["fecha_solicitud"]);
+        $nombre_usuario = test_input($_POST["nombre_usuario"]);
+        $telefono = test_input($_POST["telefono"]);
+        $correo_electronico = test_input($_POST["correo_electronico"]);
+        $descripcion = test_input($_POST["descripcion"]);
+
+        echo "<div class='container'>";
+        echo "<h3>Tu infirmación fue enviada con éxito!:</h3>";
+        echo "<p>Área Solicitante: $area_solicitante</p>";
+        echo "<p>Folio: $folio</p>";
+        echo "<p>Responsable del Área Solicitante: $responsable_area</p>";
+        echo "<p>Fecha de Solicitud: $fecha_solicitud</p>";
+        echo "<p>Nombre del Usuario: $nombre_usuario</p>";
+        echo "<p>Teléfono: $telefono</p>";
+        echo "<p>Correo Electrónico: $correo_electronico</p>";
+        echo "<p>Descripción del Servicio: $descripcion</p>";
+        echo "</div>";
+        
+    }
+    ?>
+</body>
+</html>
