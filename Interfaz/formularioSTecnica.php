e461ce93fc0c4b37055bdc2625a79831f8d11823
Paola V.B <Paola_VB@ciencias.unam.mx>
2023-10-29 21:42:01 -0600

    Base de datos avance

--- Interfaz/formularioSTecnica.php
+++ Interfaz/formularioSTecnica.php
@@ -1,3 +1,25 @@
+<?php
+$servername = "localhost"; // Reemplaza con la dirección IP o nombre del servidor remoto
+$username = "root";
+$password = "Paolataemylove25";
+$database = "Solicitudes";
+
+
+// Crear una conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos.";
+    // Aquí puedes comenzar a ejecutar consultas y trabajar con la base de datos.
+}
+$conn->close();
+
+// Resto de tu código para ejecutar consultas
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
