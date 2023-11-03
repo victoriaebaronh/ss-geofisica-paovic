0faacc23b7c6270402617d9a29ff9454e6862d4c
Paola V.B <Paola_VB@ciencias.unam.mx>
2023-11-03 08:25:49 -0600

    Avance

--- Interfaz/formularioSTecnica.php
+++ Interfaz/formularioSTecnica.php
@@ -1,25 +1,3 @@
-<?php
-$servername = "localhost"; // Reemplaza con la dirección IP o nombre del servidor remoto
-$username = "root";
-$password = "Paolataemylove25";
-$database = "Solicitudes";
-
-
-// Crear una conexión
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-} else {
-    echo "Conexión exitosa a la base de datos.";
-    // Aquí puedes comenzar a ejecutar consultas y trabajar con la base de datos.
-}
-$conn->close();
-
-// Resto de tu código para ejecutar consultas
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,15 +5,31 @@ $conn->close();
 </head>
 <body>
     <?php
-    
+    // Definir una función para validar y limpiar los datos del formulario
     function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 
+    // Verificar si el formulario ha sido enviado
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Procesar y guardar datos en la base de datos (conexión a la base de datos)
+        $servername = "localhost";
+        $username = "root";
+        $password = "Paolataemylove25";
+        $database = "Solicitudes";
+
+        // Crear una conexión a la base de datos
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        // Verificar la conexión
+        if ($conn->connect_error) {
+            die("Conexión fallida: " . $conn->connect_error);
+        }
+
+        // Limpiar y obtener los datos del formulario
         $area_solicitante = test_input($_POST["area_solicitante"]);
         $folio = test_input($_POST["folio"]);
         $responsable_area = test_input($_POST["responsable_area"]);
@@ -45,18 +39,49 @@ $conn->close();
         $correo_electronico = test_input($_POST["correo_electronico"]);
         $descripcion = test_input($_POST["descripcion"]);
 
-        echo "<div class='container'>";
-        echo "<h3>Tu infirmación fue enviada con éxito!:</h3>";
-        echo "<p>Área Solicitante: $area_solicitante</p>";
-        echo "<p>Folio: $folio</p>";
-        echo "<p>Responsable del Área Solicitante: $responsable_area</p>";
-        echo "<p>Fecha de Solicitud: $fecha_solicitud</p>";
-        echo "<p>Nombre del Usuario: $nombre_usuario</p>";
-        echo "<p>Teléfono: $telefono</p>";
-        echo "<p>Correo Electrónico: $correo_electronico</p>";
-        echo "<p>Descripción del Servicio: $descripcion</p>";
-        echo "</div>";
-        
+        // Realizar la consulta SQL para insertar datos en la base de datos
+        $sql = "INSERT INTO Solicitudes (Area_del_Solicitante, Nombre_del_Responsable, Telefono, Fecha, Correo_Electronico, Descripcion_del_Servicio, Nombre_del_Usuario) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        // Preparar la consulta
+        $stmt = $conn->prepare($sql);
+
+        if ($stmt) {
+            // Vincular los parámetros con los valores del formulario
+            $stmt->bind_param("sssssss", $area_solicitante, $responsable_area, $telefono, $fecha_solicitud, $correo_electronico, $descripcion, $nombre_usuario);
+
+            // Ejecutar la consulta
+            // Ejecutar la consulta
+            if ($stmt->execute()) {
+                // Obtener el valor del folio generado por la base de datos
+                $folioGenerado = $conn->insert_id;
+                
+                // Actualizar el campo oculto en el formulario
+                echo "<script>document.getElementById('folio').value = '$folioGenerado';</script>";
+                echo "<div class='container'>";
+                echo "<h3>Tu información fue enviada con éxito:</h3>";
+                echo "<p>Área Solicitante: $area_solicitante</p>";
+                echo "<p>Folio: $folioGenerado</p>"; // Mostrar el folio generado
+                echo "<p>Responsable del Área Solicitante: $responsable_area</p>";
+                echo "<p>Fecha de Solicitud: $fecha_solicitud</p>";
+                echo "<p>Nombre del Usuario: $nombre_usuario</p>";
+                echo "<p>Teléfono: $telefono</p>";
+                echo "<p>Correo Electrónico: $correo_electronico</p>";
+                echo "<p>Descripción del Servicio: $descripcion</p>";
+                echo "</div>";
+            } else {
+                echo "Error al insertar datos en la base de datos: " . $stmt->error;
+            }
+
+
+            // Cerrar la consulta preparada
+            $stmt->close();
+        } else {
+            echo "Error al preparar la consulta: " . $conn->error;
+        }
+
+        // Cerrar la conexión a la base de datos
+        $conn->close();
     }
     ?>
 </body>
